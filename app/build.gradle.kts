a6c72175416637fa41d2494f5508cb7746adfefd
Jahnavi-1704 <j36shah@uwaterloo.ca>
2024-02-18 16:50:59 -0500

    homescreen UI complete

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,4 +88,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
     implementation("com.google.firebase:firebase-auth")
     implementation ("androidx.browser:browser:1.7.0")
+
+    implementation("com.patrykandpatrick.vico:compose:1.13.1")
+    implementation("com.patrykandpatrick.vico:core:1.13.1")
+    implementation("com.patrykandpatrick.vico:views:1.13.1")
+    implementation("androidx.compose.material:material:1.1.1")
 }
