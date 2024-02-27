0d7942d7c9156d4b8b5f931f209b0e96109778e8
Zonggao Li <zgli@uwaterloo.ca>
2024-02-27 00:17:04 -0500

    add more bits to data model

--- app/src/main/java/com/example/architectureproject/profile/User.kt
+++ app/src/main/java/com/example/architectureproject/profile/User.kt
@@ -1,2 +1,6 @@
 package com.example.architectureproject.profile
 
+data class User(val email: String,
+                val name: String,
+                val age: Int,
+                val uid: String)
