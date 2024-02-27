d5880dd5dbe471f5225bcad56a872a7eb98709a9
Zonggao Li <zgli@uwaterloo.ca>
2024-02-27 00:14:29 -0500

    Initial user profile data model

---
+++ app/src/main/java/com/example/architectureproject/profile/UserLifestyle.kt
@@ -0,0 +1,15 @@
+package com.example.architectureproject
+
+import com.example.architectureproject.tracking.Purchase
+import com.example.architectureproject.tracking.Transportation
+
+data class UserLifestyle(val disabilities: List<Disability>,
+                         val recycledItems: List<RecyclableItem>,
+                         val preferredTransportationMode: Transportation.Mode,
+                         val allergies: List<Allergy>,
+                         val purchasingPreference: Purchase.Source
+    ) {
+    enum class Disability { /* TODO */ }
+    enum class RecyclableItem { Glass, Cardboard, Paper, Plastic }
+    enum class Allergy { Fish, Gluten, Pollen, Nut, Dairy, Soy, Egg }
+}
---
+++ app/src/main/java/com/example/architectureproject/profile/UserProvider.kt
@@ -0,0 +1,4 @@
+package com.example.architectureproject.profile
+
+class UserProvider {
+}
