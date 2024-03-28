84aa9d2fe6139a2eb76e736ccbf0268c8a04d966
Zonggao Li <zgli@uwaterloo.ca>
2024-03-27 20:45:40 -0400

    make data types java serializable for firestore api

--- app/src/main/java/com/example/architectureproject/community/CommunityChallenge.kt
+++ app/src/main/java/com/example/architectureproject/community/CommunityChallenge.kt
@@ -1,13 +1,17 @@
 package com.example.architectureproject.community
 
+import androidx.annotation.DrawableRes
+import java.io.Serializable
+
 data class CommunityChallenge(
     val id: String,
     val name: String,
     val desc: String,
     val goal: Float,
     val impact: Float,
+    @DrawableRes val icon: Int? = null,
     val randomID: String = ""
-) {
+): Serializable {
     private constructor() : this("", "", "", .0f, .0f)
 }
 
@@ -16,7 +20,7 @@ data class CommunityChallengeState(
     val active: Boolean = false,
     val lastSeen: Long = 0,
     val progress: Float = 0f
-) {
+): Serializable {
     private constructor() : this("")
 }
 
@@ -24,4 +28,6 @@ data class ChallengeProgressEvent(
     val uid: String, val date: Long,
     val challengeId: String, val name: String,
     val progressDelta: Float, val impact: Float
-)
+): Serializable {
+    private constructor() : this("", 0, "", "", 0f, 0f)
+}
