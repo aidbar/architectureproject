441ee15a6c70c822f2470f39bbcaaa73edcdccdc
aidbar <47143229+aidbar@users.noreply.github.com>
2024-04-02 11:08:40 -0400

    Refactoring `ResetPasswordScreen` to use ScreenModel

--- app/src/main/java/com/example/architectureproject/ResetPasswordScreen.kt
+++ app/src/main/java/com/example/architectureproject/ResetPasswordScreen.kt
@@ -1,7 +1,5 @@
 package com.example.architectureproject
 
-import android.content.ContentValues.TAG
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -16,62 +14,46 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import com.google.firebase.auth.FirebaseAuth
 
+class ResetPasswordScreenModel : ScreenModel {
+    var email by mutableStateOf("")
+    var result by mutableStateOf("")
+    private val auth = FirebaseAuth.getInstance()
+
+    fun resetEmail() {
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+            if(it.isSuccessful) {
+                result = "A password reset link has been sent to your email address."
+            } else {
+                if (it.exception?.message != null) {
+                    result = it.exception?.message!!
+                } else {
+                    result = "En error occurred. Please try again later."
+                }
+            }
+        }
+    }
+}
 class ResetPasswordScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.current
         val context = LocalContext.current
 
-        var email by remember { mutableStateOf("") }
+        //var email by remember { mutableStateOf("") }
+        var model = rememberScreenModel{ResetPasswordScreenModel()}
 
         val auth = FirebaseAuth.getInstance()
-
-        fun sendResetEmail(email: String) {
-            if (email.isEmpty()) {
-                Toast.makeText(
-                    context,
-                    "Enter the email associated with your account",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                auth.sendPasswordResetEmail(email).addOnCompleteListener {
-                    if(it.isSuccessful) {
-                        Log.d(TAG, "Password reset email sent successfully")
-                        Toast.makeText(
-                            context,
-                            "A password reset link has been sent to your email address. Follow that link to reset your password.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    } else {
-                        Log.e(TAG, "Failed to send password reset email", it.exception)
-                        val error = it.exception?.message
-                        if (error != null && error.contains("no user record")) {
-                            Toast.makeText(
-                                context,
-                                "Could not find a user account associated with the provided email. Please try again.",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        } else {
-                            Toast.makeText(
-                                context,
-                                "Failed to send password reset email. Please try again later.",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                }
-            }
-        }
 
         Column(
             modifier = Modifier
@@ -83,14 +65,16 @@ class ResetPasswordScreen : Screen {
             Text(text = "Update your password with just a few clicks!", style = MaterialTheme.typography.headlineMedium, modifier = Modifier.padding(start = 10.dp))
             Spacer(modifier = Modifier.height(26.dp))
             TextField(
-                value = email,
-                onValueChange = { email = it.replace(" ", "") },
+                value = model.email,
+                onValueChange = { model.email = it.replace(" ", "") },
                 label = { Text(text = "Email") }
             )
             Spacer(modifier = Modifier.height(24.dp))
             Button(
                 onClick = {
-                    sendResetEmail(email)
+                    model.resetEmail()
+                    Toast.makeText(context, model.result, Toast.LENGTH_SHORT).show()
+                    model.result = ""
                 },
                 modifier = Modifier.fillMaxWidth()
             ) {
